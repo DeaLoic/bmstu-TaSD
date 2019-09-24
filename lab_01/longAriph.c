a4bfcaf7e360e952c6dfeac190514c46afc24af1
Pavel Khetagurov <khetagurow1@yandex.ru>
2019-09-24 11:21:59 +0300

    [lab_01] Remove DEBUG print

--- lab_01/longAriph.c
+++ lab_01/longAriph.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define DEBUG_ARIPH 1
+#define DEBUG_ARIPH 0
 
 void longMultiply(longNumber* numberFirst, longNumber* numberSecond, longNumber* result, int* errorCode)
 {
